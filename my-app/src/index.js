bde1d6b48b1b6e0bee7808369023acb6e38c500a
Jui-Chen Tang <jctang@hawaii.edu>
2019-10-14 23:24:14 -1000

    Task4 not finished

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,15 +2,83 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
 import 'semantic-ui-css/semantic.min.css';
-import { Container, Header } from 'semantic-ui-react';
+import { Container, Header, Menu, Icon, Dropdown, Image} from 'semantic-ui-react';
+
+class TopMenu extends React.Component {
+  render() {
+    return (
+        <Menu borderless className="topmenu">
+        <Container>
+        <Menu.Item fitted><Icon name="facebook f" /></Menu.Item>
+        <Menu.Item fitted><Icon name="twitter" /></Menu.Item>
+        <Menu.Item fitted><Icon name="pinterest" /></Menu.Item>
+        <Menu.Item fitted><Icon name="instagram" /></Menu.Item>
+        <Menu.Item fitted position="right"><Icon name="home" /></Menu.Item>
+        <Menu.Item fitted><Icon name="search" /></Menu.Item>
+        <Menu.Item fitted><Icon name="user" /></Menu.Item>
+        <Dropdown item text="MY CART 0" icon="shop">
+        <Dropdown.Menu>
+        <Dropdown.Item>My cart is currently empty.</Dropdown.Item>
+    </Dropdown.Menu>
+    </Dropdown>
+    </Container>
+    </Menu>
+  )
+  }
+}
+
+class IslandSnowLogo extends React.Component {
+  render() {
+    return (
+    <Header as="h1">IslandSnowLogo</Header>
+  )
+  }
+}
+
+class MiddleMenu extends React.Component {
+  render() {
+    return (
+        <Menu borderless className="middlemenu">
+        <Container centered>
+        <Menu.Item> MEN </Menu.Item>
+        <Menu.Item> WOMEN </Menu.Item>
+        <Menu.Item> KIDS </Menu.Item>
+        <Menu.Item> BARANDS </Menu.Item>
+        <Menu.Item> SEARCH </Menu.Item>
+
+    </Container>
+    </Menu>
+  )
+  }
+}
+
+class FullWidthImage extends React.Component {
+  render() {
+    return (
+    <Header as="h1">FullWidthImage</Header>
+  )
+  }
+}
+
+class FooterMenu extends React.Component {
+  render() {
+    return (
+    <Header as="h1">FooterMenu</Header>
+  )
+  }
+}
 
 class IslandSnow extends React.Component {
 
   render() {
     return (
-        <Container textAlign="center">
-    <Header as='h1'>Island Snow!</Header>
-    </Container>
+        <div>
+        <TopMenu/>
+        <IslandSnowLogo/>
+        <MiddleMenu/>
+        <FullWidthImage/>
+        <FooterMenu/>
+        </div>
   );
   }
 }

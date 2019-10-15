fc1573d75ff6eb27ff52413cb4c10b824fc89625
Jui-Chen Tang <jctang@hawaii.edu>
2019-10-14 23:30:55 -1000

    Task4 not finished

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -30,7 +30,7 @@ class TopMenu extends React.Component {
 class IslandSnowLogo extends React.Component {
   render() {
     return (
-    <Header as="h1">IslandSnowLogo</Header>
+        <Image src='//cdn.shopify.com/s/files/1/1035/5187/files/logoshort_800x.png?v=1543956749' size='small' />
   )
   }
 }

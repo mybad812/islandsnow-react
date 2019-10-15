43627abcfe6b8e4ea812d72c852adf5a4db9fced
Jui-Chen Tang <jctang@hawaii.edu>
2019-10-14 23:33:47 -1000

    Task4 not finished

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -55,7 +55,7 @@ class MiddleMenu extends React.Component {
 class FullWidthImage extends React.Component {
   render() {
     return (
-    <Header as="h1">FullWidthImage</Header>
+    <Image src='//cdn.shopify.com/s/files/1/1035/5187/files/AH7I2380_180x.jpg?v=1548389088 180w, //cdn.shopify.com/s/files/1/1035/5187/files/AH7I2380_360x.jpg?v=1548389088 360w, //cdn.shopify.com/s/files/1/1035/5187/files/AH7I2380_540x.jpg?v=1548389088 540w, //cdn.shopify.com/s/files/1/1035/5187/files/AH7I2380_720x.jpg?v=1548389088 720w, //cdn.shopify.com/s/files/1/1035/5187/files/AH7I2380_900x.jpg?v=1548389088 900w, //cdn.shopify.com/s/files/1/1035/5187/files/AH7I2380_1080x.jpg?v=1548389088 1080w, //cdn.shopify.com/s/files/1/1035/5187/files/AH7I2380_1296x.jpg?v=1548389088 1296w, //cdn.shopify.com/s/files/1/1035/5187/files/AH7I2380_1512x.jpg?v=1548389088 1512w, //cdn.shopify.com/s/files/1/1035/5187/files/AH7I2380_1728x.jpg?v=1548389088 1728w, //cdn.shopify.com/s/files/1/1035/5187/files/AH7I2380_1944x.jpg?v=1548389088 1944w, //cdn.shopify.com/s/files/1/1035/5187/files/AH7I2380_2160x.jpg?v=1548389088 2160w, //cdn.shopify.com/s/files/1/1035/5187/files/AH7I2380_2376x.jpg?v=1548389088 2376w, //cdn.shopify.com/s/files/1/1035/5187/files/AH7I2380_2592x.jpg?v=1548389088 2592w, //cdn.shopify.com/s/files/1/1035/5187/files/AH7I2380_2808x.jpg?v=1548389088 2808w, //cdn.shopify.com/s/files/1/1035/5187/files/AH7I2380_3024x.jpg?v=1548389088 3024w' />
   )
   }
 }
